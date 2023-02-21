2bdaa4d6c74cf30dc81fdf6be5c3fa76016d992f
clone-of-kahless <95552552+clone-of-kahless@users.noreply.github.com>
2023-02-21 14:16:27 -0500

    Update main.py
    transform/validation step

--- main.py
+++ main.py
@@ -7,9 +7,42 @@ from datetime import datetime
 import datetime
 import sqlite3
 
+#tutorial: https://github.com/karolina-sowinska/free-data-engineering-course-for-beginners/
+
 DATABASE_LOCATION = "sqlite:///recently_played.sqlite"
-USER_ID = "sheaftw"
-TOKEN = "" # get token from https://developer.spotify.com/console/get-recently-played/
+USER_ID = "sheaftw" #your user_id
+TOKEN = "" 
+    # get token from https://developer.spotify.com/console/get-recently-played/
+
+def data_validation(df: pd.DataFrame) -> bool:
+
+    # Check if empty 
+    if df.empty:
+        print("No data found. Finishing execution.")
+        return False
+    
+    # primary key: played_at
+    # Primary Key Check - Raise Exception if duplicates
+    if pd.Series(df['played_at']).is_unique:
+        pass
+    else:
+        raise Exception("Primary key violated.")
+
+    # check for null values:
+    if df.isnull().values.any():
+        raise Exception("Null values found.")
+
+    # check data only includes that listened to/timestamps within past 24 hours
+    yesterday = datetime.datetime.now() - datetime.timedelta(days = 1)
+    yesterday = yesterday.replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+        #ready for comparison to timestamp
+    
+    timestamps = df["timestamp"].tolist()
+    for timestamp in timestamps:
+        if datetime.datetime.strptime(timestamp, "%Y-%m-%d") != yesterday:
+            raise Exception("At least one of the returned tracks not from yesterday.")
+
+    return True
 
 if __name__ == "__main__":
 
@@ -30,7 +63,8 @@ if __name__ == "__main__":
     request = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time = yesterday_unix_timestamp), headers = headers)
 
     data = request.json()
-
+    #print(data)
+    
     #extract items of interest
     track_names = []
     artist_names = []
@@ -55,3 +89,6 @@ if __name__ == "__main__":
     track_df = pd.DataFrame(track_dict, columns = ["track_name", "artist_name", "played_at", "timestamp"])
 
     #print(track_df) # looks ok (:
+
+    if data_validation(track_df):
+        print("Data validation checks passed. Proceed to load.")
